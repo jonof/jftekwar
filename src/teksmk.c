dec8d411fa86b368c960da6bccf9e18d234d931f
Jonathon Fowler <jf@jonof.id.au>
2024-10-27 19:00:02 +1000

    revisit smacker playback loop for better a/v sync
    Also downmix stereo for the credits video since JFAudiolib demand-feed
    playback takes mono.

--- src/teksmk.c
+++ src/teksmk.c
@@ -19,11 +19,12 @@
 char smk_render_frame(smk object, unsigned long frame);
 
 static smk smkmenu, smkseq;
+static unsigned char *smkmenubuf;
 #define NUMSMKTILES 3
 #define SMKPICNUM0 (MAXTILES-NUMSMKTILES)
 
 #define SMKAUDIOBUFSZ (32768)
-static char *smkaudiobuf;
+static char smkaudiobuf[SMKAUDIOBUFSZ];
 static unsigned long smkaudiowrite = 0, smkaudioread = 0;
 
 static void smkplayfeeder(char **ptr, unsigned int *length) {
@@ -35,18 +36,16 @@ static void smkplayfeeder(char **ptr, unsigned int *length) {
 void
 smkplayseq(char *name)
 {
-     char path[BMAX_PATH], result;
+     char path[BMAX_PATH], result = SMK_DONE;
      int fh, flen, i;
 
-     unsigned char smkpal[NUMSMKTILES][768], *smkbuf;
-     unsigned char *smkframe[NUMSMKTILES];
-     const unsigned char *palptr;
+     unsigned char smkpal[NUMSMKTILES][768], smkchannels[7], smkbitdepth[7], *smkbuf = NULL;
+     unsigned char *smkframes = NULL;
+     unsigned long smkx, smky, smkaudrate[7];
 
-     unsigned long xsiz, ysiz;
-     unsigned long audio_rate[7];
-     double usf, clocktime;
-
-     int voice = -1, frame, playframe, decodeframe;
+     unsigned long playframe, decodeframe;
+     double usf, clocktime, nowtime;
+     int voice = -1, frame;
 
      snprintf(path, sizeof(path), "smk/%s.smk", name);
      if ((fh = kopen4load(path, 0)) < 0) {
@@ -60,7 +59,6 @@ smkplayseq(char *name)
 
      flen = kfilelength(fh);
      smkbuf = (unsigned char *)malloc(flen);
-
      if (smkbuf == NULL) {
           kclose(fh);
           debugprintf("smkplayseq(\"%s\") malloc of %d bytes failed\n", name, flen);
@@ -77,84 +75,89 @@ smkplayseq(char *name)
           return;
      }
 
-     free(smkbuf);
-
      smk_enable_all(smkseq, SMK_VIDEO_TRACK | SMK_AUDIO_TRACK_0);
      smk_info_all(smkseq, NULL, NULL, &usf);
-     smk_info_video(smkseq, &xsiz, &ysiz, NULL);
-     smk_info_audio(smkseq, NULL, NULL, NULL, audio_rate);
+     smk_info_video(smkseq, &smkx, &smky, NULL);
+     smk_info_audio(smkseq, NULL, smkchannels, smkbitdepth, smkaudrate);
+     if (smkbitdepth[0] != 8 || smkchannels[0] > 2) {
+          debugprintf("smkplayseq(\"%s\") is not 8-bit mono/stereo audio\n", name);
+          goto end;
+     }
 
      // Smacker frames are decoded linewise, but BUILD expects its
      // tiles columnwise, so we will treat the tile as though it's
      // rotated 90 degrees and flipped horizontally.
 
-     memset(smkframe, 0, sizeof(smkframe));
+     smkframes = malloc(NUMSMKTILES * smkx*smky);
+     if (!smkframes) {
+          debugprintf("smkplayseq(\"%s\") malloc of frames failed\n", name);
+          goto end;
+     }
      for (i = 0; i < NUMSMKTILES; i++) {
-          smkframe[i] = malloc(xsiz*ysiz);
-          if (!smkframe[i]) {
-               debugprintf("smkplayseq(\"%s\") malloc of frames failed\n", name);
-               goto end;
-          }
-
-          tilesizx[SMKPICNUM0+i] = ysiz;
-          tilesizy[SMKPICNUM0+i] = xsiz;
-          waloff[SMKPICNUM0+i] = (intptr_t)smkframe[i];
+          tilesizx[SMKPICNUM0+i] = smky;
+          tilesizy[SMKPICNUM0+i] = smkx;
+          waloff[SMKPICNUM0+i] = (intptr_t)&smkframes[i*smkx*smky];
      }
 
-     smkaudiobuf = malloc(SMKAUDIOBUFSZ);
-     if (!smkaudiobuf) {
-          debugprintf("smkplayseq(\"%s\") malloc of audio buffer failed\n", name);
-          goto end;
-     }
+     smkaudiowrite = smkaudioread = 0;
+     decodeframe = playframe = 0;
+     result = smk_first(smkseq);
 
-     smkaudiowrite = 0;
-     for (decodeframe = 0, result = smk_first(smkseq);
-               decodeframe < NUMSMKTILES;
-               decodeframe++, result = smk_next(smkseq)) {
-          unsigned long audiolen;
-          const unsigned char *audiodata;
+     while (result == SMK_MORE || (playframe < decodeframe && result == SMK_LAST)) {
+          while (decodeframe - playframe < NUMSMKTILES && result == SMK_MORE) {
+               unsigned long audiolen, slice, downsamp;
+               const unsigned char *audiodata, *palptr;
 
-          if (result != SMK_MORE && result != SMK_LAST) {
-               debugprintf("smkplayseq(\"%s\") error on initial frames (%d)\n", name, result);
-               goto end;
-          }
-          for (i=0, palptr=smk_get_palette(smkseq); i<768; i++, palptr++)
-               smkpal[decodeframe][i] = (*palptr) >> 2;
-          memcpy(smkframe[decodeframe], smk_get_video(smkseq), xsiz*ysiz);
+               frame = decodeframe % NUMSMKTILES;
+               memcpy(&smkframes[frame*smkx*smky], smk_get_video(smkseq), smkx*smky);
+               for (palptr = smk_get_palette(smkseq), i=256*3-1; i>=0; i--)
+                   smkpal[frame][i] = palptr[i] >> 2;
 
-          audiolen = smk_get_audio_size(smkseq, 0);
-          audiodata = smk_get_audio(smkseq, 0);
+               audiolen = smk_get_audio_size(smkseq, 0);
+               audiodata = smk_get_audio(smkseq, 0);
+               if (smkchannels[0] == 2) audiolen >>= 1;     // Stereo is downmixed for JFAudiolib.
+               while (audiolen > 0) {
+                    if (smkaudiowrite + audiolen > SMKAUDIOBUFSZ) {
+                         slice = SMKAUDIOBUFSZ - smkaudiowrite;
+                    } else {
+                         slice = audiolen;
+                    }
+                    if (smkchannels[0] == 2) {
+                         downsamp = slice - 1;
+                         do { smkaudiobuf[smkaudiowrite+downsamp] =
+                              (audiodata[(downsamp<<1)+0] >> 1) +
+                              (audiodata[(downsamp<<1)+1] >> 1);
+                         } while (downsamp--);
+                    } else {
+                         memcpy(&smkaudiobuf[smkaudiowrite], audiodata, slice);
+                    }
+                    audiodata += slice * smkchannels[0];
+                    audiolen -= slice;
+                    smkaudiowrite = (smkaudiowrite + slice) % SMKAUDIOBUFSZ;
+               }
 
-          while (audiolen > 0) {
-               unsigned long slice = SMKAUDIOBUFSZ - smkaudiowrite;
-               if (slice > audiolen) slice = audiolen;
-               memcpy(&smkaudiobuf[smkaudiowrite], audiodata, slice);
-               audiolen -= slice;
-               audiodata += slice;
-               smkaudiowrite = (smkaudiowrite + slice) % SMKAUDIOBUFSZ;
-          }
-
-          if (result == SMK_LAST) break;
-     }
-
-     smkaudioread = 0;
-     voice = FX_StartDemandFeedPlayback(smkplayfeeder, (int)audio_rate[0],
-          0, 63, 63, 63, 1, (unsigned int)-1);
-     if (voice < 0) {
-          debugprintf("smkplayseq(\"%s\") failed to start audio playback\n", name);
-          goto end;
-     }
-
-     clocktime = (double)getusecticks();
-     playframe = 0;
-     while (result == SMK_MORE && playframe < decodeframe) {
-          while ((double)getusecticks() - clocktime > usf) {
-               clocktime += usf;
-               playframe++;
+               decodeframe++;
+               result = smk_next(smkseq);
+               if (result == SMK_ERROR) goto end;
           }
 
-          frame = playframe%NUMSMKTILES;
-          setbrightness(brightness, smkpal[frame], 2);
+          nowtime = (double)getusecticks();
+
+          if (voice < 0) {
+               voice = FX_StartDemandFeedPlayback(smkplayfeeder, (int)smkaudrate[0],
+                    0, 63, 63, 63, 1, (unsigned int)-1);
+               if (voice < 0) {
+                    debugprintf("smkplayseq(\"%s\") failed to start audio playback\n", name);
+                    goto end;
+               }
+
+               clocktime = nowtime;
+          }
+
+          while (nowtime - clocktime >= usf) clocktime += usf, playframe++;
+          frame = playframe % NUMSMKTILES;
+
+          setbrightness(-1, smkpal[frame], 2|4);
 #if USE_POLYMOST && USE_OPENGL
           invalidatetile(SMKPICNUM0+frame, 0, -1);
 #endif
@@ -162,34 +165,6 @@ smkplayseq(char *name)
           rotatesprite(160<<16, 100<<16, divscale16(200, tilesizx[SMKPICNUM0+frame]),
                       512, SMKPICNUM0+frame, 0, 0, 2+4+64, 0, 0, xdim-1, ydim-1);
           nextpage();
-
-          while ((decodeframe+1)%NUMSMKTILES != playframe%NUMSMKTILES && result != SMK_LAST) {
-               unsigned long audiolen;
-               const unsigned char *audiodata;
-
-               result = smk_next(smkseq);
-               if (result != SMK_MORE && result != SMK_LAST) {
-                    debugprintf("smkplayseq(\"%s\") smk_next error (%d)\n", name, result);
-                    break;
-               }
-               frame = decodeframe%NUMSMKTILES;
-               for (i=0, palptr=smk_get_palette(smkseq); i<768; i++, palptr++)
-                    smkpal[frame][i] = (*palptr) >> 2;
-               memcpy(smkframe[frame], smk_get_video(smkseq), xsiz*ysiz);
-
-               audiolen = smk_get_audio_size(smkseq, 0);
-               audiodata = smk_get_audio(smkseq, 0);
-               while (audiolen > 0) {
-                    unsigned long slice = SMKAUDIOBUFSZ - smkaudiowrite;
-                    if (slice > audiolen) slice = audiolen;
-                    memcpy(&smkaudiobuf[smkaudiowrite], audiodata, slice);
-                    audiolen -= slice;
-                    audiodata += slice;
-                    smkaudiowrite = (smkaudiowrite + slice) % SMKAUDIOBUFSZ;
-               }
-
-               decodeframe++;
-          }
 
           handleevents();
           if (keystatus[0x1]|keystatus[0x1c]|keystatus[0x39]|keystatus[0x9c]) {
@@ -201,26 +176,21 @@ smkplayseq(char *name)
           }
      }
 
-     FX_StopSound(voice);
 end:
+     if (result == SMK_ERROR) debugprintf("smkplayseq(\"%s\") error at decodeframe %lud (%d)\n", name, decodeframe, result);
+     if (voice >= 0) FX_StopSound(voice);
      if (smkseq) smk_close(smkseq);
      smkseq = NULL;
+     if (smkbuf) free(smkbuf);
 
-     if (smkaudiobuf) free(smkaudiobuf);
-     smkaudiobuf = NULL;
-
-     for (i = 0; i < NUMSMKTILES; i++) {
-          waloff[SMKPICNUM0+i] = 0;
-          if (smkframe[i]) free(smkframe[i]);
-          smkframe[i] = NULL;
-     }
+     for (i = 0; i < NUMSMKTILES; i++) waloff[SMKPICNUM0+i] = 0;
+     if (smkframes) free(smkframes);
 }
 
 void
 smkopenmenu(char *name)
 {
-     unsigned char *smkbuf;
-     unsigned long xsiz, ysiz;
+     unsigned long smkx, smky;
      int fh, flen;
 
      fh = kopen4load(name, 0);
@@ -230,35 +200,33 @@ smkopenmenu(char *name)
      }
 
      flen = kfilelength(fh);
-     smkbuf = (unsigned char *)malloc(flen);
+     smkmenubuf = (unsigned char *)malloc(flen);
 
-     if (smkbuf == NULL) {
+     if (smkmenubuf == NULL) {
           kclose(fh);
           debugprintf("smkopenmenu(\"%s\") malloc of %d bytes failed\n", name, flen);
           return;
      }
 
-     kread(fh, smkbuf, flen);
+     kread(fh, smkmenubuf, flen);
      kclose(fh);
 
-     smkmenu = smk_open_memory(smkbuf, flen);
+     smkmenu = smk_open_memory(smkmenubuf, flen);
      if (!smkmenu) {
-          free(smkbuf);
-          smkbuf = 0;
+          free(smkmenubuf);
+          smkmenubuf = NULL;
           debugprintf("smk_open_memory() returned null\n");
           return;
      }
-
-     free(smkbuf);
 
      // Smacker frames are decoded linewise, but BUILD expects its
      // tiles columnwise, so we will treat the tile as though it's
      // rotated 90 degrees and flipped horizontally.
 
      smk_enable_all(smkmenu, SMK_VIDEO_TRACK);
-     smk_info_video(smkmenu, &xsiz, &ysiz, NULL);
-     tilesizx[SMKPICNUM0] = ysiz;
-     tilesizy[SMKPICNUM0] = xsiz;
+     smk_info_video(smkmenu, &smkx, &smky, NULL);
+     tilesizx[SMKPICNUM0] = smky;
+     tilesizy[SMKPICNUM0] = smkx;
 }
 
 void
@@ -279,17 +247,15 @@ smkmenuframe(int fn)
      smk_render_frame(smkmenu, fn-1);
 
      // Convert the palette to the VGA (0-63) range BUILD uses.
-     palptr = smk_get_palette(smkmenu);
-     for (i=0; i<768; i++, palptr++) {
-          spal[i] = (*palptr) >> 2;
-     }
+     for (palptr = smk_get_palette(smkmenu), i=256*3-1; i>=0; i--)
+          spal[i] = palptr[i] >> 2;
 
      waloff[SMKPICNUM0] = (intptr_t)smk_get_video(smkmenu);
 #if USE_POLYMOST && USE_OPENGL
      invalidatetile(SMKPICNUM0, 0, -1);
 #endif
 
-     setbrightness(brightness, spal, 2);
+     setbrightness(-1, spal, 2|4);
 }
 
 void
@@ -308,11 +274,10 @@ smkshowmenu()
 void
 smkclosemenu()
 {
-     if (!smkmenu) {
-          return;
-     }
+     if (smkmenu) smk_close(smkmenu);
+     if (smkmenubuf) free(smkmenubuf);
 
      waloff[SMKPICNUM0] = 0;
-
-     smk_close(smkmenu);
+     smkmenu = NULL;
+     smkmenubuf = NULL;
 }
